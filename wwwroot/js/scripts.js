792e357408cb13a0c4abc807c97827ade6bfa84a
Naywia <48431873+Naywia@users.noreply.github.com>
2024-11-22 11:41:32 +0100

    Frontpage styling to look like mockup

---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,9 @@
+﻿// wwwroot/js/scripts.js
+
+window.addClassToBody = function (className) {
+    document.body.classList.add(className);
+};
+
+window.removeClassFromBody = function (className) {
+    document.body.classList.remove(className);
+};
